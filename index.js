d649aad4a0ca946fb78953e9ebd98a124d0e275e
qweruiop-oss <gbgbgbb3333@outlook.com>
2025-06-30 20:26:03 +0800

    10

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ function reveal() {
 }
 
 window.addEventListener('scroll', reveal);
+
+function toggleEmail(event) {
+    event.preventDefault();
+    const emailBtn = event.currentTarget;
+    emailBtn.classList.toggle('flipped');
+
+    // 5秒后自动翻转回来
+    setTimeout(() => {
+        emailBtn.classList.remove('flipped');
+    }, 5000);
+}
